df72caf1dbd9b7d761c94bbc364ededdb839666e
zzaighum <zzaighum@hotmail.com>
2023-01-30 23:22:02 +0500

    Initial Project Setup

---
+++ Example/ZKAnimationRecorder/ZKAnimationRecorder.h
@@ -0,0 +1,19 @@
+//
+//  ZKAnimationRecorder.h
+//  ZKAnimationRecorder
+//
+//  Created by Zaighum on 1/30/23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZKAnimationRecorder.
+FOUNDATION_EXPORT double ZKAnimationRecorderVersionNumber;
+
+//! Project version string for ZKAnimationRecorder.
+FOUNDATION_EXPORT const unsigned char ZKAnimationRecorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZKAnimationRecorder/PublicHeader.h>
+
+
